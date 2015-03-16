8034167d7ba75b757635fdc7e11a4ae10f555585
Ben Reinhart <benjreinhart@gmail.com>
2015-03-16 14:49:39 -0500

    Clean up URL handling a bit

--- lib/support_bee/client_interface.rb
+++ lib/support_bee/client_interface.rb
@@ -2,7 +2,7 @@ module SupportBee
   module ClientInterface
 
     def ticket(id)
-      url = build_url("tickets/#{ id }?auth_token=#{ auth_token }")
+      url = build_url("/tickets/#{ id }")
 
       RestClient.get(url) do |response, request, result, &block|
         case response.code
@@ -17,7 +17,7 @@ module SupportBee
     end
 
     def create_ticket(params)
-      RestClient.post(build_url('tickets'), { ticket: params }, build_headers) do |response, request, result, &block|
+      RestClient.post(build_url("/tickets", include_auth_token: false), { ticket: params }, build_headers) do |response, request, result, &block|
         case response.code
         when 201
           format_response(parse_json(response)).ticket
@@ -30,7 +30,7 @@ module SupportBee
     end
 
     def add_label(ticket_id, label)
-      url = build_url("tickets/#{ ticket_id }/labels/#{ label }?auth_token=#{ auth_token }")
+      url = build_url("/tickets/#{ ticket_id }/labels/#{ label }")
 
       RestClient.post(url, {}, build_headers) do |response, request, result, &block|
         case response.code
@@ -44,10 +44,18 @@ module SupportBee
       end
     end
 
+
     private
 
-    def build_url(endpoint)
-      "https://#{company}.supportbee.com/" + endpoint
+    def build_url(endpoint, include_auth_token: true)
+      uri = URI.parse(company_url)
+      uri.path = endpoint
+
+      if include_auth_token
+        uri.query = URI.encode_www_form(auth_token: auth_token)
+      end
+
+      uri.to_s
     end
 
     def format_response(parsed_json)
@@ -60,6 +68,10 @@ module SupportBee
 
     def build_headers(headers={})
       { content_type: :json, accept: :json }.merge(headers)
+    end
+
+    def company_url
+      @company_url ||= "https://#{company}.supportbee.com/".freeze
     end
   end
 end
